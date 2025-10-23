2234f127d0403e80d7f05bc6e0b9e2f5252c2aac
Mehari Bereket <bereketmehari68@gmail.com>
2025-10-23 13:37:23 +0800

    Setup radix UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from './NavBar'
+import "@radix-ui/themes/styles.css";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Theme } from "@radix-ui/themes";
+import NavBar from './NavBar';
+
+;
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,10 +21,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      
       <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+  </Theme>
       </body>
+
     </html>
   )
 }
