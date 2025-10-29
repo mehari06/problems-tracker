29592dbc7827aa44ae3b9d2403fff79328aba6f6
Mehari Bereket <bereketmehari68@gmail.com>
2025-10-30 06:49:11 +0800

    change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import { usePathname } from 'next/navigation';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
 import { Status } from '@prisma/client';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const NavBar = () => {
     const {status,data:session}=useSession();
@@ -19,9 +19,12 @@ const NavBar = () => {
   ]
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><AiFillBug /></Link>
-        <ul className='flex space-x-6'>
+    <nav className='border-b mb-5 px-5 py-3'>
+      <Container>
+       <Flex justify="between">
+        <Flex align="center" gap="3">
+            <Link href="/"><AiFillBug /></Link>
+          <ul className='flex space-x-6'>
           {links.map(link=>
           <li  key={link.href} > 
           <Link 
@@ -36,10 +39,14 @@ const NavBar = () => {
            href={link.href}>{link.label}</Link></li>)}
             
          </ul>
-         <Box>
+        </Flex>
+          <Box>
           {status==="authenticated" && ( <Link href="api/auth/signout">Logout</Link> )}
            {status==="unauthenticated" && ( <Link href="api/auth/signin">Login</Link> )}
          </Box>
+       </Flex>
+       </Container>
+      
     </nav>
   )
 }
