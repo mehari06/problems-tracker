16327b044d73123143a1fd17ee7824bbf345112d
Mehari Bereket <bereketmehari68@gmail.com>
2025-10-31 12:43:18 +0800

    Refactor the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,3 +1,76 @@
+// 'use client'
+// import Link from 'next/link'
+// import React from 'react'
+// import { AiFillBug } from "react-icons/ai";
+// import { usePathname } from 'next/navigation';
+// import classnames from 'classnames';
+// import { useSession } from 'next-auth/react';
+// import { Status } from '@prisma/client';
+// import { Avatar, Box, Container, DropdownMenu, Flex ,Text} from '@radix-ui/themes';
+
+// const NavBar = () => {
+//     const {status,data:session}=useSession();
+//    const currentPath= usePathname();
+//    console.log(currentPath)
+//   const links =[
+//     {label:'Dashboard',href:'/'},
+//      {label:'Issues',href:'/issues'}
+
+//   ]
+
+//   return (
+//     <nav className='border-b mb-5 px-5 py-3'>
+//       <Container>
+//        <Flex justify="between">
+//         <Flex align="center" gap="3">
+//             <Link href="/"><AiFillBug /></Link>
+//           <ul className='flex space-x-6'>
+//           {links.map(link=>
+//           <li  key={link.href} > 
+//           <Link 
+//           className={
+//             classnames({
+//               'text-zinc-900':currentPath===link.href,
+//               'text-zinc-500':currentPath!==link.href,
+//               'hover:text-zinc-800 transition-colors':true
+          
+//             })
+//           }
+//            href={link.href}>{link.label}</Link></li>)}
+            
+//          </ul>
+//         </Flex>
+//           <Box>
+//           {status==="authenticated" && (
+//             <DropdownMenu.Root>
+//                <DropdownMenu.Trigger>
+//                 <Avatar src={session.user!.image!} size="2" fallback="?"  radius="full" className="cursor-pointer"/>
+//                </DropdownMenu.Trigger>
+//                <DropdownMenu.Content>
+//                  <DropdownMenu.Label>
+//                   <Text size="2">
+//                   {session.user?.email}
+//                    </Text>
+//                  </DropdownMenu.Label>
+//                  <DropdownMenu.Item>
+//                 <Link href="api/auth/signout">Logout</Link>
+//             </DropdownMenu.Item>
+//                </DropdownMenu.Content>
+//             </DropdownMenu.Root>
+//              )}
+//            {status==="unauthenticated" && ( <Link href="api/auth/signin">Login</Link> )}
+//          </Box>
+//        </Flex>
+//        </Container>
+      
+//     </nav>
+//   )
+// }
+// export default NavBar
+
+
+//new
+
 'use client'
 import Link from 'next/link'
 import React from 'react'
@@ -5,67 +78,88 @@ import { AiFillBug } from "react-icons/ai";
 import { usePathname } from 'next/navigation';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Status } from '@prisma/client';
-import { Avatar, Box, Container, DropdownMenu, Flex ,Text} from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 const NavBar = () => {
-    const {status,data:session}=useSession();
-   const currentPath= usePathname();
-   console.log(currentPath)
-  const links =[
-    {label:'Dashboard',href:'/'},
-     {label:'Issues',href:'/issues'}
 
-  ]
 
-  return (
-    <nav className='border-b mb-5 px-5 py-3'>
-      <Container>
-       <Flex justify="between">
-        <Flex align="center" gap="3">
-            <Link href="/"><AiFillBug /></Link>
-          <ul className='flex space-x-6'>
-          {links.map(link=>
-          <li  key={link.href} > 
-          <Link 
-          className={
-            classnames({
-              'text-zinc-900':currentPath===link.href,
-              'text-zinc-500':currentPath!==link.href,
-              'hover:text-zinc-800 transition-colors':true
-          
-            })
-          }
-           href={link.href}>{link.label}</Link></li>)}
-            
-         </ul>
-        </Flex>
-          <Box>
-          {status==="authenticated" && (
-            <DropdownMenu.Root>
-               <DropdownMenu.Trigger>
-                <Avatar src={session.user!.image!} fallback="?" size="2" radius='full' 
-                className='cursor-pointer'
-                />
-               </DropdownMenu.Trigger>
-               <DropdownMenu.Content>
-                 <DropdownMenu.Label>
-                  <Text size="2">
-                  {session.user?.email}
-                   </Text>
-                 </DropdownMenu.Label>
-                 <DropdownMenu.Item>
-                <Link href="api/auth/signout">Logout</Link>
-            </DropdownMenu.Item>
-               </DropdownMenu.Content>
-            </DropdownMenu.Root>
-             )}
-           {status==="unauthenticated" && ( <Link href="api/auth/signin">Login</Link> )}
-         </Box>
-       </Flex>
-       </Container>
-      
-    </nav>
-  )
+    return (
+        <nav className='border-b mb-5 px-5 py-3'>
+            <Container>
+                <Flex justify="between">
+                    <Flex align="center" gap="3">
+                        <Link href="/"><AiFillBug /></Link>
+                        <NavLinks />
+                        
+                    </Flex>
+                    <AuthStatus />
+                </Flex>
+            </Container>
+        </nav>
+    )
 }
+  const NavLinks=()=>{
+     const currentPath = usePathname();
+    console.log(currentPath)
+    
+    const links = [
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' }
+    ]
+    return(
+    <ul className='flex space-x-6'>
+                            {links.map(link =>
+                                <li key={link.href}>
+                                    <Link
+                                        className={
+                                            classnames({
+                                                'text-zinc-900': currentPath === link.href,
+                                                'text-zinc-500': currentPath !== link.href,
+                                                'hover:text-zinc-800 transition-colors': true
+                                            })
+                                        }
+                                        href={link.href}>{link.label}
+                                        </Link>
+
+                                </li>)}
+                        </ul>
+    )
+  }
+
+
+const AuthStatus = ()=>{
+   const { status, data: session } = useSession();
+   if (status==="loading") return null;
+   if(status==="unauthenticated") return <Link href="/api/auth/signin">Login</Link>
+
+  return <Box>
+
+                            <DropdownMenu.Root>
+                                <DropdownMenu.Trigger>
+                                    <Avatar 
+                                        src={session!.user!.image!} 
+                                        size="2" 
+                                        fallback="?" 
+                                        radius="full" 
+                                        className="cursor-pointer"
+                                        referrerPolicy="no-referrer"
+                                    />
+                                </DropdownMenu.Trigger>
+                                <DropdownMenu.Content>
+                                    <DropdownMenu.Label>
+                                        <Text size="2">
+                                            {session!.user?.email}
+                                        </Text>
+                                    </DropdownMenu.Label>
+                                    <DropdownMenu.Separator />
+                                    <DropdownMenu.Item>
+                                        <Link href="/api/auth/signout">Logout</Link>
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Content>
+                            </DropdownMenu.Root>
+                      
+                       
+                    </Box>
+}
+
 export default NavBar
